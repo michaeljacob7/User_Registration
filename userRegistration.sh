3d96e85c79a6c4e274770b13c82b8241dc075fbf
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-15 11:02:31 +0530

    First name validation added

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,4 +2,21 @@
 
 echo "Welcome to User Registration Program"
 
+function firstName()
+{
+	echo "Enter the first word"
+	read word
+	pattern="^[a-zA-z]{3,}"
 
+	if [[ $word =~ $pattern ]]
+	then
+		echo "valid first name"
+	else
+		echo "invalid first name"
+	fi
+}
+function main()
+{
+firstName
+}
+main
